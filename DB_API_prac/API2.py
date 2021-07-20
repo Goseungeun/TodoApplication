6961a8e3358fd1bc15994cf72277620113fdbe10
Goseungeun <74888933+Goseungeun@users.noreply.github.com>
2021-07-20 11:04:35 +0900

    Delete API2.py

--- DB_API_prac/API2.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,request,jsonify,current_app
-from flask.json import JSONEncoder
-from sqlalchemy import create_engine, Text
-
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, set):
-            return list(obj)
